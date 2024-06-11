12bd527293591d0edda5cac656b3a344c81ad9d7
Thiago Martins <thiago.fmartins@outlook.com>
2024-06-10 21:08:31 -0300

    removing datetime precision from post pub date

--- src/generate_rss.py
+++ src/generate_rss.py
@@ -58,7 +58,7 @@ def generate_rss():
                 fe.title(title)
                 fe.link(href=f'./posts/{filename.replace(".md", "")}')
                 fe.description(description)
-                fe.pubDate(pub_date.strftime("%a, %d %b %Y %H:%M:%S +0000"))
+                fe.pubDate(pub_date.strftime("%a, %d %b %Y"))
 
     fg.rss_file(os.path.join(docs_folder, "rss.xml"))
 
